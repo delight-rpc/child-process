3243419701645127f9316d6c7593cbe145b469a4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-12-04 02:30:18 +0000

    test: add cases

--- __tests__/main-as-client-and-child-process-as-server/index.spec.ts
+++ __tests__/main-as-client-and-child-process-as-server/index.spec.ts
@@ -1,9 +1,10 @@
-import { createClient } from '@src/client.js'
+import { createBatchClient, createClient } from '@src/client.js'
 import { fork, ChildProcess } from 'child_process'
 import { IAPI } from './contract.js'
 import * as path from 'path'
 import { getErrorPromise } from 'return-style'
 import { fileURLToPath } from 'url'
+import { createBatchProxy } from 'delight-rpc'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -27,12 +28,36 @@ describe('Main as Client, ChildProcess as Server', () => {
     expect(result).toStrictEqual('hello')
   })
 
+  test('echo (batch)', async () => {
+    const [client, close] = createBatchClient(childProcess)
+    const proxy = createBatchProxy<IAPI>()
+
+    const result = await client.parallel(proxy.echo('hello'))
+    close()
+
+    expect(result.length).toBe(1)
+    expect(result[0].unwrap()).toBe('hello')
+  })
+
   test('error', async () => {
     const [client, close] = createClient<IAPI>(childProcess)
 
     const err = await getErrorPromise(client.error('hello'))
     close()
 
+    expect(err).toBeInstanceOf(Error)
+    expect(err!.message).toMatch('hello')
+  })
+
+  it('error (batch)', async () => {
+    const [client, close] = createBatchClient(childProcess)
+    const proxy = createBatchProxy<IAPI>()
+
+    const result = await client.parallel(proxy.error('hello'))
+    close()
+
+    expect(result.length).toBe(1)
+    const err = result[0].unwrapErr()
     expect(err).toBeInstanceOf(Error)
     expect(err!.message).toMatch('hello')
   })
